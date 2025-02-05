709ce70d21ba3e4bf7a3fe948f07659c1720d056
Sushil Yadav <sushil7161@gmail.com>
2025-02-05 12:22:05 +0000

    exercise 2

--- 01_02b/main.go
+++ 01_02b/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"time"
+	"strings"
 )
 
 const delay = 700 * time.Millisecond
@@ -16,7 +17,17 @@ func print(msg string) {
 // slowDown takes the given string and repeats its characters
 // according to their index in the string.
 func slowDown(msg string) {
-	panic("NOT IMPLEMENTED")
+	// result := make([]string, 0)
+	// result = append(result, string(msg[0]))
+	slice := strings.Split(msg, " ")
+	for _, w := range slice {
+		pw := make([]string, 0)
+		for i, wb := range w {
+			rep := strings.Repeat(string(wb), i + 1)
+			pw = append(pw, rep)
+		}
+		print(strings.Join(pw, ""))
+	}
 }
 
 func main() {
