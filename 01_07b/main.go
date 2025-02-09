2c424667777fc96e54c4eade4c3c6d4033da7a8d
Sushil Yadav <sushil7161@gmail.com>
2025-02-09 16:42:07 +0000

    exercise 7

--- 01_07b/main.go
+++ 01_07b/main.go
@@ -5,10 +5,65 @@ import (
 	"log"
 )
 
+type operatorType int
+const (
+	openBracket operatorType = iota
+	closedBracket
+	otherOperator
+)
+var bracketsMap = map[rune]rune {
+	'{': '}',
+	'(': ')',
+	'[': ']',
+}
+
+func getOperatorType(op rune) operatorType {
+	for ob, cb := range bracketsMap {
+		switch op {
+		case ob:
+			return openBracket
+		case cb:
+			return closedBracket
+		}
+	}
+	return otherOperator
+}
+
+// create a stack
+type stack struct {
+	items []rune
+}
+
+func (s *stack) push(op rune) {
+	s.items = append(s.items, op)
+}
+
+func (s *stack) pop() *rune {
+	n := len(s.items) - 1
+	last := s.items[n]
+	s.items = s.items[:n]
+	return &last
+}
+
 // isBalanced returns whether the given expression
 // has balanced brackets.
 func isBalanced(expr string) bool {
-	panic("NOT IMPLEMENTED")
+	s := stack{}
+	for _, e := range expr {
+		switch (getOperatorType(e)) {
+		case openBracket:
+			s.push(e)
+		case closedBracket:
+			if len(s.items) == 0 {
+				return false
+			}
+			last := s.pop()
+			if last == nil || bracketsMap[*last] != e {
+				return false
+			}
+		}
+	}
+	return len(s.items) == 0
 }
 
 // printResult prints whether the expression is balanced.
